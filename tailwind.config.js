9c61a746bc5dd887a766daf0bd3c6a3679d00230
edwardcastle <eduardo.castillo@teavaro.com>
2023-02-22 11:43:21 -0500

    CE-1 feat: Add screen responsiveness configuration on tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,17 @@ module.exports = {
     "./src/**/*.md",
     "./app.vue",
   ],
+  theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      xs: "100px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1230px",
+      "2xl": "1536px",
+    },
+  },
 };
