1e200e907cf67aa9d71dbd83d4438ea113b39a21
edwardcastle <eduardo.castillo@teavaro.com>
2023-02-25 20:00:45 -0500

    CE-2 refactor: Remove unnecessary package.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import "./assets/main.css";
 import App from "./App.vue";
 import router from "./router";
-import "@oruga-ui/oruga-next/dist/oruga.css";
-import Oruga from "@oruga-ui/oruga-next";
 
-const app = createApp(App).use(Oruga);
+const app = createApp(App);
 
-app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
